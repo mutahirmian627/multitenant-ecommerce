4168faf3ff87dec24d31da1fa9458aec54b9f05d
Muhammad Mutahir <muhammad@shahtez.com>
2026-05-11 18:31:29 +0500

    01:Setup

---
+++ mulitenant-ecommerce/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-rose-400">
+        Hello World
+      </p>
+    </div>
+  );
+}
