d2c7119321506988e3de8295d1985708ff85d248
Muhammad Mutahir <muhammad@shahtez.com>
2026-05-26 19:19:35 +0500

    05:Search filters

---
+++ mulitenant-ecommerce/src/app/(app)/(home)/Search-filters/subcategory-menu.tsx
@@ -0,0 +1,43 @@
+import { Category } from "@/payload-types";
+import Link from "next/link";
+
+interface props {
+    isOpen: boolean,
+    category: Category,
+    position: { top: number, left: number }
+}
+
+export const SubcategoryMenu = ({isOpen, category, position}: props) => {
+    if(!isOpen || !category.subcategories || category.subcategories.length === 0){
+        return null;
+    };
+
+    const backgroundColor = category.color || "#F5F5F5";
+    return (
+        <div className="fixed z-100"
+        style={{ 
+            top: position.top,
+            left: position.left,
+         }}
+        >
+            {/* Invisible bridge to maintain hover */}
+            <div className="h-3 w-60"/>
+            <div
+            style={{ backgroundColor }}
+            className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-0.5 -translate-y-0.5">
+                <div>
+                    {category.subcategories.map((subcategory: Category) => (
+                        <Link 
+                        key={subcategory.slug} 
+                        href="/"
+                        className="underline font-medium items-center hover:text-white hover:bg-black justify-between p-4 text-left w-full flex"
+                        >
+                            {subcategory.name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+
+        </div>
+    )
+}
