f3ae4af5a66d0c11e3e5b3ce725417aa12ba82e5
Muhammad Mutahir <muhammad@shahtez.com>
2026-06-09 17:40:28 +0500

    11:Products

--- mulitenant-ecommerce/src/trpc/routers/_app.ts
+++ mulitenant-ecommerce/src/trpc/routers/_app.ts
@@ -1,10 +1,13 @@
-import { authRouter } from '@/modules/auth/server/procedures';
 import { createTRPCRouter } from '../init';
+
+import { authRouter } from '@/modules/auth/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
+import { productsRouter } from '@/modules/products/server/procedures';
  
 export const appRouter = createTRPCRouter({
   auth: authRouter,
-  categories: categoriesRouter
+  categories: categoriesRouter,
+  products: productsRouter,
 });
  
 // export type definition of API
