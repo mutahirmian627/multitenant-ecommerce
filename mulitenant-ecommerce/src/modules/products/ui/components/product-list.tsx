8374386ce8d9662df3a9b11ab0dc8628f83982ed
Muhammad Mutahir <muhammad@shahtez.com>
2026-06-10 21:46:53 +0500

    13:Api Filters Sorting

--- mulitenant-ecommerce/src/modules/products/ui/components/product-list.tsx
+++ mulitenant-ecommerce/src/modules/products/ui/components/product-list.tsx
@@ -1,18 +1,25 @@
 "use client";
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { UseProductFilters } from "../../hooks/use-product-filters";
 
 interface Props {
-    category?:string
+    category?: string
 }
 
-export const ProductList = ({category}: Props) => {
+export const ProductList = ({ category }: Props) => {
+
+    const [filters] = UseProductFilters();
+
     const trpc = useTRPC();
-    const { data } = useSuspenseQuery(trpc.products.getMany.queryOptions({category}));
+    const { data } = useSuspenseQuery(trpc.products.getMany.queryOptions({
+        category,
+        ...filters,
+    }));
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-            {data?.docs.map((product)=>(
+            {data?.docs.map((product) => (
                 <div key={product.id} className="p-4 border rounded-md bg-white">
                     <h2 className="text-xl font-medium">
                         {product.name}
--- mulitenant-ecommerce/src/trpc/routers/_app.ts
+++ mulitenant-ecommerce/src/trpc/routers/_app.ts
@@ -3,8 +3,10 @@ import { createTRPCRouter } from '../init';
 import { authRouter } from '@/modules/auth/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { productsRouter } from '@/modules/products/server/procedures';
+import { tagsRouter } from '@/modules/tags/server/procedures';
  
 export const appRouter = createTRPCRouter({
+  tags: tagsRouter,
   auth: authRouter,
   categories: categoriesRouter,
   products: productsRouter,
